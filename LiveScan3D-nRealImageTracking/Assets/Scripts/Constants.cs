591a24b0071d1314eebc1468e7ee25dd1db43a20
Eric Liu <eric1932@users.noreply.github.com>
2022-01-04 20:49:11 -0600

    Update editor version to 2020.3.25f1

--- LiveScan3D-nRealImageTracking/Assets/Scripts/Constants.cs
+++ LiveScan3D-nRealImageTracking/Assets/Scripts/Constants.cs
@@ -7,7 +7,8 @@ public class Constants : MonoBehaviour
 #if UNITY_EDITOR
     public static string serverHostName = "127.0.0.1";
 #else
-    public static string serverHostName = "139.180.141.82";
+    //public static string serverHostName = "139.180.141.82";
+    public static string serverHostName = "127.0.0.1";
 #endif
 
     public static int port = 48002;
