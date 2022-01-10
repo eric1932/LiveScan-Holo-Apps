44a79da2570565f3e4e932a1186a03787629a004
Eric Liu <eric1932@users.noreply.github.com>
2022-01-10 00:55:19 -0600

    Update Constants.cs
    Optimize IP configuration

--- LiveScan3D-nRealImageTracking/Assets/Scripts/Constants.cs
+++ LiveScan3D-nRealImageTracking/Assets/Scripts/Constants.cs
@@ -6,11 +6,13 @@ public class Constants : MonoBehaviour
 {
     public static readonly string localhost = "127.0.0.1";
     public static readonly string VultrSG = "139.180.141.82";
-    public static readonly string LAN = "192.168.10.14";
+    public static readonly string LANTUF = "192.168.10.14";
+    public static readonly string LANEsxi = "192.168.10.10";
 
     public static string serverHostName =
 #if UNITY_EDITOR
-        localhost
+        //localhost
+        LANEsxi
 #else
         VultrSG
 #endif
