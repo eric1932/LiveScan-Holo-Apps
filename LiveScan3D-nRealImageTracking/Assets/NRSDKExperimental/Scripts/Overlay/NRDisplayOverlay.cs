7eef3b1c531dcd5a852412b4fa868e83da02755a
Eric Liu <eric1932@users.noreply.github.com>
2022-07-26 01:03:21 -0500

    Upgrade NRSDK to 1.8.0Exp
    (cherry picked from commit bb55b2a13dd878ea8e53cd6e5606c86e4577305f)

---
+++ LiveScan3D-nRealImageTracking/Assets/NRSDKExperimental/Scripts/Overlay/NRDisplayOverlay.cs
@@ -0,0 +1,123 @@
+﻿/****************************************************************************
+* Copyright 2019 Nreal Techonology Limited. All rights reserved.
+*                                                                                                                                                          
+* This file is part of NRSDK.                                                                                                          
+*                                                                                                                                                           
+* https://www.nreal.ai/        
+* 
+*****************************************************************************/
+
+namespace NRKernal.Experimental
+{
+    using System;
+    using UnityEngine;
+
+    [ScriptOrder(NativeConstants.NRRENDER_ORDER)]
+    [RequireComponent(typeof(Camera))]
+    public class NRDisplayOverlay : OverlayBase
+    {
+        /// <summary>
+        /// Which display this overlay should render to.
+        /// </summary>
+        [Tooltip("Which display this overlay should render to.")]
+        public NativeDevice targetEye;
+
+        private Camera m_RenderCamera;
+        private int m_EyeWidth;
+        private int m_EyeHeight;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            m_RenderCamera = gameObject.GetComponent<Camera>();
+
+#if !UNITY_EDITOR
+            NRDevice.Instance.Init();
+            var resolution = NRFrame.GetDeviceResolution(NativeDevice.LEFT_DISPLAY);
+            m_EyeWidth = resolution.width;
+            m_EyeHeight = resolution.height;
+#else
+            m_EyeWidth = 1920;
+            m_EyeHeight = 1080;
+#endif
+            this.compositionDepth = int.MaxValue;
+            m_BufferSpec.size = new NativeResolution((int)m_EyeWidth, (int)m_EyeHeight);
+            m_BufferSpec.colorFormat = NRColorFormat.NR_COLOR_FORMAT_RGBA_8888;
+            m_BufferSpec.depthFormat = NRDepthStencilFormat.NR_DEPTH_STENCIL_FORMAT_DEPTH_24;
+            m_BufferSpec.samples = 1;
+            m_BufferSpec.useExternalSurface = false;
+        }
+
+        public override void SwapBuffers(IntPtr bufferHandler)
+        {
+            if (!Textures.ContainsKey(bufferHandler))
+            {
+                NRDebugger.Error("[NRDisplayOverlay] Can not find the texture:" + bufferHandler);
+                return;
+            }
+
+            Texture targetTexture;
+            Textures.TryGetValue(bufferHandler, out targetTexture);
+            RenderTexture renderTexture = targetTexture as RenderTexture;
+            if (renderTexture == null)
+            {
+                NRDebugger.Error("[NRDisplayOverlay] The texture is null...");
+                return;
+            }
+            m_RenderCamera.targetTexture = renderTexture;
+        }
+
+        public override void CreateViewport()
+        {
+            m_ViewPorts = new ViewPort[1];
+            m_ViewPorts[0].reprojection = NRReprojectionType.NR_REPROJECTION_TYPE_FULL;
+            m_ViewPorts[0].sourceUV = new Rect(0, 0, 1, 1);
+            m_ViewPorts[0].targetDisplay = targetEye == NativeDevice.LEFT_DISPLAY ? LayerSide.Left : LayerSide.Right;
+            m_ViewPorts[0].layerID = m_LayerId;
+            NRFrame.GetEyeFov(targetEye, ref m_ViewPorts[0].fov);
+            m_ViewPorts[0].is3DLayer = true;
+            NRSwapChainManager.Instance.CreateBufferViewport(ref m_ViewPorts[0]);
+        }
+
+        public override void UpdateViewPort()
+        {
+            if (m_ViewPorts == null)
+            {
+                NRDebugger.Warning("Can not update view port for this layer:{0}", gameObject.name);
+                return;
+            }
+
+            NRSwapChainManager.Instance.UpdateBufferViewport(ref m_ViewPorts[0]);
+        }
+
+        public override void CreateOverlayTextures()
+        {
+            NRDebugger.Info("[NRDisplayOverlay] Create DefaultEyeTextures. ");
+            ReleaseOverlayTextures();
+            for (int i = 0; i < m_BufferSpec.bufferCount; i++)
+            {
+                RenderTexture texture = UnityExtendedUtility.CreateRenderTexture((int)m_EyeWidth, (int)m_EyeHeight, 24, RenderTextureFormat.ARGB32);
+                IntPtr texturePtr = texture.GetNativeTexturePtr();
+                Textures.Add(texturePtr, texture);
+            }
+        }
+
+        public override void ReleaseOverlayTextures()
+        {
+            if (Textures.Count == 0)
+            {
+                return;
+            }
+
+            NRDebugger.Info("[NRDisplayOverlay] ReleaseOverlayTextures. ");
+            foreach (var item in Textures)
+            {
+                RenderTexture rt = item.Value as RenderTexture;
+                if (rt != null) rt.Release();
+            }
+
+            Textures.Clear();
+        }
+    }
+}
